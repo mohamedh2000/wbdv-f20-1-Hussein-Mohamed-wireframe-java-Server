89ed90b25c7dcc5a40d45c832fa55489bb4fb46e
Hussein Mohamed <mohamed.h@husky.neu.edu>
2020-09-26 23:30:35 -0400

    finished

---
+++ src/main/webapp/services/user.service.client.js
@@ -0,0 +1,53 @@
+function AdminUserServiceClient() {
+
+
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    let url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/users';
+    var self = this;
+
+    function createUser(user) {
+        return fetch(url, {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => {return response.json()})
+    };
+
+    function findAllUsers() {
+        return fetch(url).then((response) => {
+            return response.json();
+        });
+    };
+
+    function updateUser(userId, user) {
+        return fetch(url + "/" + userId, {
+            method: "PUT",
+            body: JSON.stringify(user),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => {return response.json()});
+    }
+
+
+    function deleteUser(userId) {
+        return fetch(url + "/" + userId, {
+            method: "DELETE"
+        }).then(response => {return response.json()});
+    }
+
+    function findUserById(userId) {
+        return fetch(url + "/" + userId).then((user) => {
+            console.log(user.body);
+            return user.json();
+        });
+     };
+
+
+}
--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,3 +1,4 @@
+
 let users =[
     {
         username: "alice",
@@ -18,15 +19,14 @@ let users =[
         role: "FACULTY"
     }
 ]
-
 function renderUsers (users) {
     $body.empty();
     console.log(users);
     for(let i = 0; i < users.length; i++) {
          const user = users[i];
          const username = user.username;
-         const fName = user.fName;
-         const lName = user.lName;
+         const fName = user.first;
+         const lName = user.last;
          const role = user.role;
          let $templateT = $template.clone();
          console.log($templateT)
@@ -34,35 +34,10 @@ function renderUsers (users) {
          $templateT.find(".wbdv-first-name").html(fName);
          $templateT.find(".wbdv-last-name").html(lName);
          $templateT.find(".wbdv-role").html(role);
-         $templateT.find(".wbdv-remove").click(() => deleteUser2(i));
-         $templateT.find(".wbdv-edit").click(() => updateUser(i));
+         $templateT.find(".wbdv-remove").click(() => deleteUser(i));
+         $templateT.find(".wbdv-edit").click(() => findUserById(i));
          $(".wbdv-tbody").append($templateT);
     }
-};
-
-const deleteUser = (event) => {
-    console.log(event);
-    const deleteBtn = event.currentTarget;
-    const $deleteBtn = $(deleteBtn).parents("tr");
-    $deleteBtn.remove();
-    alert("deletedUser!");
-};
-
-const deleteUser2 = (index) => {
-    users.splice(index, 1);
-    renderUsers(users);
-};
-
-const updateUser = (index) => {
-    username = users[index].username;
-    fName = users[index].fName;
-    lName = users[index].lName;
-    role = users[index].role;
-    $(".wbdv-form").find("#usernameFld").val(username);
-    $(".wbdv-form").find("#firstNameFld").val(fName);
-    $(".wbdv-form").find("#lastNameFld").val(lName);
-    $(".wbdv-form").find("#roleFld").val(role);
-    console.log($(".rolefld"))
 };
 
 let $template = $(".wbdv-template");
@@ -71,7 +46,111 @@ let $body = $("tbody.wbdv-tbody");
 function init() {
     $template = jQuery(".wbdv-template");
     $body = $("tbody.wbdv-tbody");
+    $(".wbdv-create").click(createUser);
+    $(".wbdv-update").click()
+    findAllUsers();
     renderUsers(users);
 }
 
-jQuery(init);
+    var $usernameFld, $passwordFld;
+    var $removeBtn, $editBtn, $createBtn;
+    var $firstNameFld, $lastNameFld, $roleFld;
+    var $userRowTemplate, $tbody;
+    var userService = new AdminUserServiceClient();
+    $(main);
+
+    function main() {
+        init(); //it does essentially the same thing as init. I can't initialize anything else in
+        //main or init due to them needing to be specific to each row/listing
+    }
+
+    function createUser() {
+        const username = $("#usernameFld").val();
+        const first = $("#firstNameFld").val();
+        const last = $("#lastNameFld").val();
+        const newUser = {
+            username: username,
+            first: first,
+            last: last
+        }
+        userService.createUser(newUser).then(actualInsertedUser => {
+            users.push(actualInsertedUser);
+            renderUsers(users);
+            console.log(users);
+        });
+
+    }
+    function findAllUsers() {
+        userService.findAllUsers().then((_users) => {
+            console.log(_users);
+            users = _users;
+            renderUsers(users);
+        });
+    };
+
+    const deleteUser = (index) => {
+        userService.deleteUser(users[index]._id).then(status => {console.log(status)});
+        users.splice(index, 1)
+        renderUsers(users);
+    };
+
+    function updateUser(id) {
+        user = {        _id:id,
+                        username:
+                           $(".wbdv-form").find("#usernameFld").val(),
+                        first:
+                           $(".wbdv-form").find("#firstNameFld").val(),
+                        last:
+                           $(".wbdv-form").find("#lastNameFld").val()
+                       };
+
+        userService.updateUser(id, user).then(data => {
+            renderUser(user);
+        });
+    };
+
+    function findUserById(id) {
+        user = "";
+        userService.findUserById(users[id]._id).then((_user) => {
+            user = _user;
+            username = user.username;
+            fName = user.first;
+            lName = user.last;
+            $(".wbdv-form").find("#usernameFld").val(username);
+            $(".wbdv-form").find("#firstNameFld").val(fName);
+            $(".wbdv-form").find("#lastNameFld").val(lName);
+            $(".wbdv-update").click(() => updateUser(users[id]._id));
+        });
+
+    };
+
+function renderUser(user) {
+console.log(user);
+    id = user._id;
+    console.log(id);
+    for (i = 0; i < users.length; i ++) {
+        if ( users[i]._id == id) {
+            users[i].first = user.first;
+            users[i].last = user.last;
+            users[i].username = user.username;
+            renderUsers(users);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
